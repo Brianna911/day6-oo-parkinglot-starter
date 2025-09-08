413c5fc3f847c3a38d7fe3836a6c54d9972abe82
YANGBR2 <yangbr2@oocl.com>
2025-09-08 21:32:45 +0800

    test:Multiple vehicles are parked and retrieved simultaneously, ensuring that the correct vehicles are returned.

--- src/test/java/com/afs/parkinglot/ParkingLotTest.java
+++ src/test/java/com/afs/parkinglot/ParkingLotTest.java
@@ -25,4 +25,19 @@ public class ParkingLotTest {
         assertNotNull(fetchedCar);
         assertEquals(car, fetchedCar);
     }
+    @Test
+    public void testFetchTwoCarsWithTwoTickets() {
+        ParkingLot parkingLot = new ParkingLot(10);
+        Car car1 = new Car("CAR001");
+        Car car2 = new Car("CAR002");
+
+        Ticket ticket1 = parkingLot.park(car1);
+        Ticket ticket2 = parkingLot.park(car2);
+
+        Car fetchedCar1 = parkingLot.fetch(ticket1);
+        Car fetchedCar2 = parkingLot.fetch(ticket2);
+
+        assertEquals(car1, fetchedCar1);
+        assertEquals(car2, fetchedCar2);
+    }
 }
